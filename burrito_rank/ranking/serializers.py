d83095a94f61650dd58c554f57be1bdf432675e0
Bruno Fuentes <bruno.fuentes@alunos.ifsuldeminas.edu.br>
2024-11-12 18:45:36 -0300

    Delete burrito_rank directory

--- burrito_rank/ranking/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Score
-
-class ScoreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Score
-        fields = ['player_name', 'points', 'time_spent', 'date_played']
